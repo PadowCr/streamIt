280d699717891ef5ed83d0cea424ddc2ce9718d0
PadowCr <padow.corporation@gmail.com>
2024-01-05 15:07:36 +0100

    Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,58 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: "StreamIt | Films, séries, animés",
+
+      htmlAttrs: {
+        lang: 'fr'
+      },
+
+      meta: [
+        {name: 'description', content: "SreamIt est une application web de visionnage de films, séries ou animés"}
+      ],
+
+      link: [
+        {rel: 'icon', type: 'image/png', href: '/Logo.png' },
+        { rel: 'stylesheet', href: 'https://unicons.iconscout.com/release/v4.0.8/css/line.css'}
+      ],
+
+      script: [
+        { src: "https://kit.fontawesome.com/4ca1d285a7.js", crossorigin:"anonymous"},
+      ]
+    }
+  },
+
+  css: [
+  ],
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    }
+  ],
+  
+  modules: [
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss'
+
+  ],
+
+  googleFonts: {
+    families: {
+      Rubik: {
+        wght: [400, 500, 600, 700, 800]
+      },
+
+      Fredoka: {
+        wght: [700]
+      },
+    },
+    display: 'swap'
+  }
 })
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+export default {
+    content: [
+    ],
+
+    theme: {
+      extend: {
+        colors: {
+            "background": '#0D0C0F',
+            "button": '#28262D',
+
+            "grey2": '#78828A',
+            "grey1": '#9CA4AB',
+            "white": '#F9FAFB',
+
+            "purple": '#7C3AED',
+            "red": '#EB3F5E',
+            "green": '#00925D',
+
+            "hover-purple": '#9333EA',
+            "hover-red": '#EB3349'
+        },
+
+        fontFamily: {
+            'rubik': ['Rubik', 'sans-serif'],
+            'fredoka': ['Fredoka', 'sans-serif']
+        }
+      }
+    },
+
+    plugins: [],
+}
